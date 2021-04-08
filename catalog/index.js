6111e69225bcaff00fee0e819f1db2208c37f3dd
eisenils <mail.eisenils@gmail.com>
2021-04-08 18:39:41 +0200

    Make a PWA (hopefully) 😅

--- catalog/index.js
+++ catalog/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Catalog, pageLoader } from "catalog";
 import {theme} from "./theme"
 import Logo from "./static/ne-dsgn_logo.svg"
@@ -61,3 +62,8 @@ ReactDOM.render(
   />,
   document.getElementById("catalog")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
